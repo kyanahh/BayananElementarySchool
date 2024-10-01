789efed88ce6e92224f2c2498f4b644c55d2537e
Kianna Dominique Alvarez <119477262+kyanahh@users.noreply.github.com>
2024-10-01 18:09:27 +0800

    changed pass

--- contacts.php
+++ contacts.php
@@ -27,14 +27,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com'; // Replace with your SMTP server
         $mail->SMTPAuth = true;
-        $mail->Username = 'calagodenbert_bsit@plmun.edu.ph'; // Replace with your email
-        $mail->Password = 'nurm zexy krps rweo'; // Replace with your email password
+        $mail->Username = 'denbertcalago@gmail.com'; // Replace with your email
+        $mail->Password = 'oqoh dqni gljr avzu'; // Replace with your email password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption
         $mail->Port = 587;
 
         //Recipients
         $mail->setFrom($email, $name);
-        $mail->addAddress('calagodenbert_bsit@plmun.edu.ph', 'BESMAIN Website'); // Change as needed
+        $mail->addAddress('denbertcalago@gmail.com', 'BESMAIN Website'); // Change as needed
 
         //Content
         $mail->isHTML(true);
